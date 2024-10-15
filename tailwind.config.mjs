802b71b98ea1290b6f2324de6686754be6eca56d
DaniLobo2610 <danilobo2018@gmail.com>
2024-10-15 14:02:58 -0600

    initial Portafolio settings

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				'dark-blue': '#1E3A8A',
+				'Light-blue':'#3B82F6',
+				'grey': '#ADC2EB',
+			}
+		}
+	},
+	plugins: [],
+}
